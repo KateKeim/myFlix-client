25feef09c5e4d600afec307be62f7c12fe96e167
KateKeim <110600575+KateKeim@users.noreply.github.com>
2023-04-01 17:10:15 +0200

    Authentization React app

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,46 +1,99 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { useEffect } from "react";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch("https://openlibrary.org/search.json?q=star+wars")
+    if (!token) {
+    return;
+  }
+
+    setLoading(true);
+    fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((data) => {
-        const moviesFromApi = data.docs.map((doc) => {
+
+        setLoading(false);
+        console.log('data', data);
+        const MoviesFromAPI = data.map((movie) => {
           return {
             id: doc.key,
             title: doc.title,
-            image:
-`https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
             author: doc.author_name?.[0]
           };
         });
-
-        setMovies(moviesFromApi);
+        setMovies(MoviesFromAPI);
       });
-  }, []);
+  }, [token]);
+
+
+  if (!user) {
+    return (
+      <>
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+      or
+      <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
+      <>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear();
+      }}
+      > Logout
+      </button>
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      </>
     );
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear();
+      }}
+      > Logout
+      </button>
+      <div>The list is empty!</div>
+    </>
+    );
   }
 
   return (
+    loading ? (
+      <p>Loading...</p>
+    ) : !movies || !movies.length ? (
+      <p>No movies found</p>
+    ) : (
     <div>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear();
+      }}
+    > Logout
+    </button>
+    
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id}
+          key={movie._id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
@@ -48,5 +101,5 @@ export const MainView = () => {
         />
       ))}
     </div>
-  );
-};
+  ));
+}
